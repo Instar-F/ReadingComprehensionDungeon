0cc3996393074facfdd835f35b78e68ea4eee610
Instar-F <Instar-F>
2025-12-14 21:30:04 +0200

    fixed: used wrong css file

--- auth/signup.php
+++ auth/signup.php
@@ -36,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Registrera - Lärportal</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="../assets/css/laro-style.css">
+  <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body class="bg-light">
   <div class="container py-5">
